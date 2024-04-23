9772054711bf621a968fd79bdf5535bc2ef9d2b5
ajit3570 <ajit3570@CSL0685.global.chainsys.com>
2024-04-23 17:20:32 +0530

    add the file

--- day10/src/com/chainsys/day10/GroceryApp.java
+++ day10/src/com/chainsys/day10/GroceryApp.java
@@ -1,24 +1,30 @@
 package com.chainsys.day10;
 
 public class GroceryApp {
-	public void purchase(String userName) {
-		System.out.println("Enter the username:" + userName);
+	
+	public static void purchase(String userName) {
+		System.out.println();
+		if (userName.matches("^[A-Za-z]+[#@$%^]+[0-9]+$")) {
+			System.out.println("valid ");
+		} else
+			System.out.println("Invalid");
+
 	}
 
 	public static void purchase(double items, int qty, int price)
 
-	{
+	{   System.out.println();
 		System.out.println("How many items: " + items + "  TotalQty: " + qty + " Price of Rs: " + price);
 	}
 
 	public static void purchase(int productId, String productName) {
-		System.out.println("Enter the product id: " + productId + " Enter the product name: " + productName);
+		System.out.println("Product id: " + productId + " Product name: " + productName);
 	}
 
 	public static void purchase(int price, int qty, int totalPrice) {
 		totalPrice = qty * price;
 		System.out.println("totalPrice:" + totalPrice);
-		if (totalPrice < price) {
+		if (totalPrice > price) {
 			System.out.println("valid");
 		} else
 			System.out.println("Invalid");
--- day10/src/com/chainsys/day10/TestProduct.java
+++ day10/src/com/chainsys/day10/TestProduct.java
@@ -4,41 +4,155 @@ import java.util.Scanner;
 
 public class TestProduct {
 
-
-
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		GroceryApp groceryapp = new GroceryApp();
 
-		System.out.println("Welcome to all the user of  this APP");
-		System.out.println("Enter the user name:");
-		String username = sc.next();
-		System.out.println("Enter the password:");
-		String password = sc.next();
-		System.out.println("Enter the How many items:");
-		double items = sc.nextDouble();
-		System.out.println("Enter the total qty:");
-		int qty = sc.nextInt();
-		System.out.println("Enter the price Rs:");
-		int price = sc.nextInt();
-		System.out.println("Enter the productId:");
-		int productId = sc.nextInt();
-		System.out.println("Enter the productName:");
-		String ProductName = sc.next();
-		
+		System.out.println("Welcome to all the user of  this GroceryApp");
+		System.out.println();
+		System.out.println("1) Login page\n2) Signup page");
+		int n = sc.nextInt();
+		if (n == 1) {
+			System.out.println();
+			System.out.println("Enter the user name:(ex:ajith@2001)");
+			String userName = sc.next();
+			while(true) {
+			if (userName.equals("ajith@123")) {
+				System.out.println("valid");
+				System.out.println("Enter the  password:");
+				String password = sc.next();
+				double items = 2;
+//				while (items > 2) {
+//					System.out.println("we only have 2 items ");
+//					items = sc.nextDouble();
+//				}
+				System.out.println("list of items");
+				System.out.println("1.snackes");
+				System.out.println("2.vegetable");
+				System.out.println("3.fruits");
+				System.out.println("choice the items");
+				int choice = sc.nextInt();
+				System.out.println("1.biscuits");
+				System.out.println("2.cholates");
+				System.out.println("Enter the total qty:");
+				int qty = sc.nextInt();
+				System.out.println("Enter the price Rs:");
+				int price = sc.nextInt();
+				System.out.println("Enter the productId:");
+				int productId = sc.nextInt();
+				System.out.println("Enter the productName:");
+				String productName = sc.next();
+				System.out.println("add to card :" + productName);
+
+				groceryapp.purchase(items, qty, price);
+				groceryapp.purchase(productId, productName);
+				int totalPrice = 0;
+				groceryapp.purchase(price, qty, totalPrice);
+				break;
+
+			} else {
+				System.out.println("Invaid(add the special character and numbers)");
+				
+				 userName = sc.next();
+
+			}
+			}
+
+		}
+		System.out.println();
 		System.out.println("Thanks for visiting  the GroceryApp");
 
-		groceryapp.purchase(username);
-		groceryapp.purchase(items, qty, price);
-		groceryapp.purchase(productId, ProductName);
-		int totalPrice = 0;
-		groceryapp.purchase(price, qty, totalPrice);
-
 		sc.close();
-
 	}
 }
+
+
+
+
+		
+		
+		
+		
+		
+		
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+//		System.out.println("Enter the How many items:");
+//      double items = 2;
+//		while (items > 2) {
+//			System.out.println("we only have 2 items ");
+//			items = sc.nextDouble();
+//		}
+		/*
+		 * System.out.println("list of items"); System.out.println("1.snackes");
+		 * System.out.println("2.vegetable"); System.out.println("3.fruits");
+		 * System.out.println("choice the items"); int choice=sc.nextInt();
+		 * System.out.println("Enter the total qty:"); int qty = sc.nextInt();
+		 * System.out.println("Enter the price Rs:"); int price = sc.nextInt();
+		 * System.out.println("Enter the productId:"); int productId = sc.nextInt();
+		 * System.out.println("Enter the productName:"); String productName = sc.next();
+		 * System.out.println("add to card :" + productName);
+		 * 
+		 * groceryapp.purchase(items, qty, price); groceryapp.purchase(productId,
+		 * productName); int totalPrice = 0; groceryapp.purchase(price, qty,
+		 * totalPrice);
+		 * 
+		 * 
+		 * 
+		 * 
+		 * System.out.println();
+		 * System.out.println("Thanks for visiting  the GroceryApp");
+		 */
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
